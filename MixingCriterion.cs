2b3aba12dcadf94b06314f2144a9685e2d346813
Matt Gibbs <gibbs.ms@gmail.com>
2019-04-08 14:37:12 +0930

    Changes to how depth is calculated in mixing criterion Pass radiation parameters into function

--- MixingCriterion.cs
+++ MixingCriterion.cs
@@ -11,25 +11,24 @@ namespace CustomFunctions
     [CustomFunctionContainer]
     public class MixingCriterion
     {
-        [CustomFunction("Stratification", "Calculate mixing criterion from Bormans and Webster (1997).\nInputs are flow (ML/d), surface area (ha), storage volume (ML), and length (m) of reach.\nUnits must match above, not handled automatically\n45,000 is the treshold to disrupt stratification")]
-        public static double Stratification(double flow,double area,double volume,double length)
+        [CustomFunction("Stratification", "Calculate mixing criterion from Bormans and Webster (1997).\nInputs are flow (ML/d), water surface elevation (m AHD), storage volume (ML), length of reach (m), bottom elevation (m AHD) of reach, net longwave radiation (W/m2) and shortwave radiation (W/m2).\nUnits must match above, not handled automatically\nDepth needs to be a \"reach average\". It is suggested to determine this using WL_NPL_m-vol_NPL_m3/area_NPL_m2\n45,000 is the treshold to disrupt stratification")]
+        public static double Stratification(double flow,double WSL,double volume,double length,double bottom_el,double Qnet,double Qi)
         {
-            double Qnet = 169.435868; //total raditation, W/m2
-            double Qi = 371; //shortwave radiation, W/m2 CURRENTLY Qnet and Qi are hard coded to Renmark in December. Needs to be made dynamic, use pattern or something
+            //double Qnet = 169.435868; //total raditation, W/m2
+            //double Qi = 371; //shortwave radiation, W/m2 CURRENTLY Qnet and Qi are hard coded to Renmark in December. Needs to be made dynamic, use pattern or something
             double kd = 2.5; //light attenuation, /m
             double a = 0.00021; //thermal expansion coefficient
             double g = 9.81; //gravity, m/s/s
             double p = 998; //density of water, kg/m3
             double Cp = 4180; //specific heat of water J/kgC
             
-            //convert units to SI
-            flow = flow / 86.4;
-            area = area * 10000.0;
-            volume = volume * 1000.0;
+            //convert units to SI - is there a type that provides access to units, rather than double?
+            double flowSI = flow / 86.4;
+            double volumeSI = volume * 1000.0;
 
             //reach averaged velocity and depth
-            double velocity = flow / (volume / length);
-            double depth = volume / area;
+            double velocity = flowSI / (volumeSI / length);
+            double depth = WSL - bottom_el;
 
             double R;
 
